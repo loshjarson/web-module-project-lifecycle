00387bdff77ea0b5b180b434a6fb11a3cc3e5028
loshjarson <jlarson129@outlook.com>
2021-08-09 00:46:00 -0400

    added styling

--- github-user-card/src/components/UserCard.js
+++ github-user-card/src/components/UserCard.js
@@ -1,5 +1,4 @@
 import React from "react";
-import axios from "axios";
 
 
 class UserCard extends React.Component {
@@ -11,31 +10,32 @@ class UserCard extends React.Component {
 
     render() {
         return (
-            <a href={this.props.user.html_url}>
+            
             <div className="user-card">
-                <div className="card-header">
-                    <h2 className="user-login">{this.props.user.login}</h2>
-                    <p className="user-id">id: {this.props.user.id}</p>
-                    <div className="stats">
-                        <p>followers:{this.props.user.followers}</p>
-                        <p>following:{this.props.user.followers}</p>
-                        <p>public repos:{this.props.user.public_repos}</p>
+                <a href={this.props.user.html_url}>
+                    <div className="card-header">
+                        <h2 className="user-login">{this.props.user.login}</h2>
+                        <p className="user-id">id: {this.props.user.id}</p>
+                        <div className="stats">
+                            <p>followers:{this.props.user.followers}</p>
+                            <p>following:{this.props.user.followers}</p>
+                            <p>public repos:{this.props.user.public_repos}</p>
+                        </div>
                     </div>
-                </div>
-                <img src={this.props.user.avatar_url} alt="user's avatar"/>
-                <div>
+                    <img src={this.props.user.avatar_url} alt="user's avatar"/>
+                    </a>
                     <h3>Followers</h3>
-                    {this.props.followers.map(user => {
-                        return(
-                            <div className="follower-card">
-                                <img src={user.avatar_url}/>
-                                <p>{user.login}</p>
-                            </div>
-                        )
-                    })}
-                </div>
+                    <div className="followers">
+                        {this.props.followers.map(user => {
+                            return(
+                                <a href={user.url} className="follower-card">
+                                        <img src={user.avatar_url}/>
+                                        <p>{user.login}</p>
+                                </a>
+                            )
+                        })}
+                    </div>
             </div>
-            </a>
         )
     }
 }
